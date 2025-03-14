299e418f7993b077466f17d9557ea712cb873a12
mr-developer01 <rahulpratik1212@gmail.com>
2025-03-14 18:12:25 +0530

    feat: modal

---
+++ src/components/pages/CreatePostPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CreatePost from "../core/CreatePost";
+
+const CreatePostPage = () => {
+  return (
+    <>
+      <CreatePost />
+    </>
+  );
+};
+
+export default CreatePostPage;
--- src/components/routes/AppRoute.tsx
+++ src/components/routes/AppRoute.tsx
@@ -6,7 +6,7 @@ import UserProfile from "../core/UserProfile";
 import Connects from "../core/Connects";
 import { Container } from "@mui/material";
 import UpdateUserProfile from "../pages/UpdateUserProfile";
-import CreatePost from "../core/CreatePost";
+import CreatePostPage from "../pages/CreatePostPage";
 
 const AppRoute = () => {
   return (
@@ -19,7 +19,7 @@ const AppRoute = () => {
             <Route path="/profile" element={<UserProfile />} />
             <Route path="/connects" element={<Connects />} />
             <Route path="/user/profile" element={<UpdateUserProfile />} />
-            <Route path="/create-post" element={<CreatePost />} />
+            <Route path="/create-post" element={<CreatePostPage />} />
           </Route>
         </Routes>
       </Container>
